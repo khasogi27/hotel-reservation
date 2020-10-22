9c767f86845bf3a488f20a10827d7a373c40d14e
khasogi27 <khasogi27@live.com>
2020-10-23 02:51:46 +0700

    deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = require('./routers/index.js')
 const session = require('express-session');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs')
 app.use(session({
